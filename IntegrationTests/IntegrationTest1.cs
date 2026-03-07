1f8a3aa46a2cc2f536f93b4eb35e0c8c321052b4
{harvest7777} <{333harvest.moon333@gmail.com}>
2026-03-06 23:16:06 -0800

    chore: remove dead tests

--- IntegrationTests/IntegrationTest1.cs
+++ IntegrationTests/IntegrationTest1.cs
@@ -37,34 +37,6 @@ public class WeatherForecastIntegrationTests
     public Task DisposeAsync() => Task.CompletedTask;
 
     [Fact]
-    public async Task Should_Insert_One_Record()
-    {
-        await using var context = new ApplicationDbContext(_fixture.Options);
-
-        context.WeatherForecasts.Add(new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.UtcNow),
-            TemperatureC = 20,
-            Summary = "Mild"
-        });
-
-        await context.SaveChangesAsync();
-
-        var count = await context.WeatherForecasts.CountAsync();
-
-        Assert.Equal(1, count);
-    }
-    [Fact]
-    public async Task Weather_Should_Be_Empty_Initially()
-    {
-        await using var context = new ApplicationDbContext(_fixture.Options);
-
-        var count = await context.WeatherForecasts.CountAsync();
-
-        Assert.Equal(0, count);
-    }
-
-    [Fact]
     public async Task Post_Should_Insert_One_Record()
     {
         var newWeatherDto = new WeatherForecastDto
