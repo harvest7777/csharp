33674b8fbfc1c8bd74716fc1b4eac2485e7d7945
harvest7777 <333harvest.moon333@gmail.com>
2026-03-28 15:35:42 -0700

    refactor: private setters, article interface
    Signed-off-by: harvest7777 <333harvest.moon333@gmail.com>

--- SwagApi/Article.cs
+++ SwagApi/Article.cs
@@ -10,16 +10,27 @@ public class Article
 {
     public int Id { get; set; }   // Primary Key
 
-    public ArticleStatus Status { get; set; } =  ArticleStatus.Draft;
+    public ArticleStatus Status { get; private set; } =  ArticleStatus.Draft;
 
-    public DateOnly? PublishedAt { get; set; }
+    public DateOnly? PublishedAt { get; private set; }
 
-    public DateOnly UpdatedAt { get; set; }
+    public DateOnly UpdatedAt { get; private set; }
 
-    public string? Content { get; set; }
+    public string? Content { get; private set; }
 
-    public string? Title { get; set; }
+    public string? Title { get; private set; }
 
-    public string? Slug { get; set; }
+    public string? Slug { get; private set; }
+
+    public void Update(string? title, string? content, string? slug)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Publish()
+    {
+        throw new NotImplementedException();
+    }
+}
 
 }
