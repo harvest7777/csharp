41ea1bea003f089e0b81fd48ef96dd3fc3cf897e
harvest7777 <333harvest.moon333@gmail.com>
2026-03-28 15:57:17 -0700

    tests: article methods
    Signed-off-by: harvest7777 <333harvest.moon333@gmail.com>

--- SwagApi.UnitTests/Articles/Article.cs
+++ SwagApi.UnitTests/Articles/Article.cs
@@ -1,22 +1,65 @@
 namespace SwagApi.UnitTests.Articles;
 
-public class UnitTest1
+public class ArticleTests
 {
+    [Fact]
+    public void Update_ShouldThrow_WhenTooLongTitle()
+    {
+        // Arrange
+        var article = new Article();
+        var longTitle = new string('x', 256);
+
+        // Assert
+        Assert.Throws<ArgumentException>(() => article.Update(longTitle, null, null));
+    }
+
+    [Fact]
+    public void Update_ShouldSet_WhenValid()
+    {
+        // Arrange
+        var article = new Article();
+
+        // Act
+        article.Update("My Title", "Some content", "my-title");
+
+        // Assert
+        Assert.Equal("My Title", article.Title);
+        Assert.Equal("Some content", article.Content);
+        Assert.Equal("my-title", article.Slug);
+    }
+
     [Fact]
     public void Publish_ShouldThrow_WhenMissingTitle()
     {
+        // Arrange
         var article = new Article();
+
+        // Assert
         Assert.Throws<InvalidOperationException>(() => article.Publish());
     }
 
     [Fact]
     public void Publish_ShouldThrow_WhenMissingContent()
     {
+        // Arrange
+        var article = new Article();
+        article.Update("My Title", null, null);
 
+        // Assert
+        Assert.Throws<InvalidOperationException>(() => article.Publish());
     }
 
     [Fact]
     public void Publish_ShouldSetStatus_WhenValid()
     {
+        // Arrange
+        var article = new Article();
+        article.Update("My Title", "Some content", "my-title");
+
+        // Act
+        article.Publish();
+
+        // Assert
+        Assert.Equal(ArticleStatus.Published, article.Status);
     }
 }
