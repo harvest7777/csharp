19f423449e5eb13fccf4d9b6d80a6aa409b42f27
harvest7777 <333harvest.moon333@gmail.com>
2026-03-29 17:17:19 -0700

    feat: add GetById endpoint and fix CreatedAtAction in Post

---
+++ SwagApi/Controllers/ArticleController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SwagApi.DTOs;
+using SwagApi.Data;
+
+namespace SwagApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ArticleController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public ArticleController(ApplicationDbContext context)
+    {
+        _context = context;
+    } 
+
+    [HttpGet(Name = "GetArticles")]
+    public async Task<ActionResult<ArticleDto[]>> Get()
+    {
+        var res = await _context.Articles.ToArrayAsync();
+        return Ok(res);
+    }
+
+    [HttpGet("{id}", Name = "GetArticle")]
+    public async Task<ActionResult<ArticleDto>> GetById(int id)
+    {
+        var article = await _context.Articles.FindAsync(id);
+        if (article is null) return NotFound();
+        return Ok(article);
+    }
+
+    [HttpPost(Name = "PostArticle")]
+    public async Task<ActionResult<ArticleDto>> Post([FromBody] PostArticleDto dto)
+    {
+        var newArticle = new Article();
+        newArticle.Update(dto.Title, dto.Content, dto.Slug);
+        _context.Add(newArticle);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetById), new { id = newArticle.Id }, newArticle);
+    }
+}
