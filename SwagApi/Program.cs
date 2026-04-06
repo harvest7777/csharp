b70392cbad60c154250de011ee1125d6f4419454
harvest7777 <333harvest.moon333@gmail.com>
2026-04-05 23:46:26 -0700

    feat: add authorization service to builder
    Signed-off-by: harvest7777 <333harvest.moon333@gmail.com>

--- SwagApi/Program.cs
+++ SwagApi/Program.cs
@@ -1,7 +1,8 @@
-using System.Net;
 using System.Threading.RateLimiting;
-using SwagApi.Data;
+using Auth0.AspNetCore.Authentication.Api;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using SwagApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,17 @@ var connectionString =
                                            + "'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
+
+builder.Services.AddAuth0ApiAuthentication(options =>
+{
+    options.Domain = builder.Configuration["Auth0:Domain"];
+    options.JwtBearerOptions = new JwtBearerOptions
+    {
+        Audience = builder.Configuration["Auth0:Audience"]
+    };
+});
+
+builder.Services.AddAuthorization();
 
 builder.Services.AddRateLimiter(options =>
 {
@@ -42,6 +54,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
