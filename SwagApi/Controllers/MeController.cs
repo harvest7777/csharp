9bbb15a8858d22d5f6fddbf789c2dd1c15e16150
harvest7777 <333harvest.moon333@gmail.com>
2026-04-06 21:43:39 -0700

    fix: redundant async
    Signed-off-by: harvest7777 <333harvest.moon333@gmail.com>

--- SwagApi/Controllers/MeController.cs
+++ SwagApi/Controllers/MeController.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using SwagApi.Data;
 
 namespace SwagApi.Controllers;
 
@@ -8,16 +7,9 @@ namespace SwagApi.Controllers;
 [Route("[controller]")]
 public class MeController : ControllerBase
 {
-    private readonly ApplicationDbContext _context;
-
-    public MeController(ApplicationDbContext context)
-    {
-        _context = context;
-    } 
-
     [Authorize]
     [HttpGet(Name = "GetMe")]
-    public async Task<ActionResult<User>> Get()
+    public ActionResult<User> Get()
     {
         User? user = HttpContext.Items["CurrentUser"] as User;
         if (user == null)
